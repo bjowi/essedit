5b0273d8cfac3c246836f57b69b1b34469309401
Björn Wingman <bjorn.wingman@gmail.com>
2011-11-06 22:09:41 +0100

    Started on write().

--- essedit.py
+++ essedit.py
@@ -3,7 +3,7 @@
 import argparse
 import datetime
 from collections import namedtuple
-from struct import unpack
+from struct import unpack, pack
 import sys
 import ImageFile
 import Image
@@ -13,6 +13,7 @@ def get_options():
     parser.add_argument('-f', '--essfile', dest='essfile', type=str)
     parser.add_argument('-i', '--image', dest='image', type=str)
     parser.add_argument('-p', '--list_plugins', dest='list_plugins', action='store_true')
+    parser.add_argument('-w', '--write_to', dest='write_to', type=str)
     return parser.parse_args()
 
 def enum(**nums):
@@ -49,29 +50,50 @@ RecordTypes, RecordTypeNames = enum(FACT=6,
                                     PACK=61)
 
 def time_from_win_systemtime(systemtime):
+    print "1win:       %r" % systemtime
     systemtime[7] *= 1000 # ms to us
     systemtime.pop(2) # Remove dayofweek
+    print "1datetime:  %r" % systemtime
     return datetime.datetime(*systemtime)
+
+def time_to_win_systemtime(pythontime):
+    systemtime = list(pythontime.timetuple())
+    print "2datetime:  %r" % systemtime
+    dow = systemtime.pop(6) # Get dayofweek
+    systemtime.insert(2, dow+1 % 6)
+    systemtime[7] /= 1000 # ms to us
+    print "2win:       %r" % systemtime[:8]
+    return systemtime[:8]
+
 
 def parse_systemtime(filehandle):
     t = unpack('8H', filehandle.read(16))
     return time_from_win_systemtime(list(t))
 
 
+def write_b_or_bzstring(filehandle, s, bz=False):
+    # Write a string prefixed with a byte length and optionally terminated with a zero (\x00).
+    if bz:
+        filehandle.write(pack('B', len(s)+1))
+        filehandle.write(s+'\x00')
+    else:
+        filehandle.write(pack('B', len(s)))
+        filehandle.write(s)
+
+
 def parse_b_or_bzstring(filehandle, bz=False):
-    # A string prefixed with a byte length and terminated with a zero (\x00).
+    # A string prefixed with a byte length and optionally terminated with a zero (\x00).
     length = unpack('B', filehandle.read(1))[0]
     s = unpack('%ds' % length, filehandle.read(length))[0]
     if bz:
-        assert s[-1] == '\x00'
-        return s[:-1]
+        print '%r' % s
+        return s
     else:
         return s
 
 
 def parse_screenshot(filehandle, write_to_file=None):
     size, width, height = unpack('3I', filehandle.read(12))
-    print [size, width, height]
     size -= 8
     rgb_data = filehandle.read(size)
     im = Image.frombuffer('RGB', (width, height), rgb_data, 'raw', 'RGB', 0, 1)
@@ -82,7 +104,6 @@ def parse_screenshot(filehandle, write_to_file=None):
 
 def parse_globals(filehandle):
     n_globals = unpack('H', filehandle.read(2))[0]
-    print "n_globals %r" % n_globals
     globals_dict = dict()
     for n in range(n_globals):
         iref, value = unpack('If', filehandle.read(8))
@@ -107,45 +128,44 @@ def parse_bytelist(filehandle, bytetype='s'):
 def parse_createddata(filehandle):
     createdNum = unpack('I', filehandle.read(4))[0]
     records = list()
-    print "Found %d created records" % createdNum
+    #print "Found %d created records" % createdNum
     for count in range(createdNum):
         record_type = unpack('4s', filehandle.read(4))
         #print 'type: %r' % record_type
         record_size = unpack('4I', filehandle.read(16))
         #print 'size: %r' % list(record_size)
         data = unpack('%ds' % record_size[0], filehandle.read(record_size[0]))
-#        print 'data: %r' % data
+        #print 'data: %r' % data
         records.append((record_type, data))
 
     return [createdNum, records]
 
 def parse_quickkeydata(filehandle):
     quickKeysSize = unpack('H', filehandle.read(2))[0]
-    print "qs %r" % quickKeysSize
     quickKeysData = filehandle.read(quickKeysSize)
 
     return [quickKeysSize, quickKeysData]
 
-    quickKeys = list()
-    bytes_read = 0
-    while True:
-        if bytes_read >= quickKeysSize:
-            break
+    # quickKeys = list()
+    # bytes_read = 0
+    # while True:
+    #     if bytes_read >= quickKeysSize:
+    #         break
 
-        flag = unpack('B', filehandle.read(1))[0]
-        bytes_read += 1
-        if flag:
-            iref = unpack('I', filehandle.read(4))[0]
-            quickKeys.append(iref)
-            bytes_read += 4
-        else:
-            notset = unpack('B', filehandle.read(1))[0]
-            quickKeys.append(notset)
-            bytes_read += 1
-        print "qs %r" % quickKeys
-        print "qs %r" % bytes_read
+    #     flag = unpack('B', filehandle.read(1))[0]
+    #     bytes_read += 1
+    #     if flag:
+    #         iref = unpack('I', filehandle.read(4))[0]
+    #         quickKeys.append(iref)
+    #         bytes_read += 4
+    #     else:
+    #         notset = unpack('B', filehandle.read(1))[0]
+    #         quickKeys.append(notset)
+    #         bytes_read += 1
+    #     print "qs %r" % quickKeys
+    #     print "qs %r" % bytes_read
 
-    return [quickKeysSize, quickKeys]
+    # return [quickKeysSize, quickKeys]
 
 def parse_regions(filehandle):
     size, count = unpack('2H', filehandle.read(4))
@@ -173,11 +193,14 @@ def load(options):
         headerpart = list(unpack('12s B B', essfile.read(14)))
         headerpart.append(parse_systemtime(essfile))
         h = FileHeader._make(headerpart)
+        print h
 
         # SaveGameHeader
         gameheader = list(unpack('3I', essfile.read(12)))
-        gameheader.append(parse_b_or_bzstring(essfile, bz=True))
+        print gameheader[-1]
 
+        gameheader.append(parse_b_or_bzstring(essfile, bz=True))
+        print gameheader[-1]
         gameheader.append(unpack('H', essfile.read(2))[0])
 
         gameheader.append(parse_b_or_bzstring(essfile, bz=True))
@@ -214,23 +237,18 @@ def load(options):
 
         globalslist.append(unpack('I', essfile.read(4))[0])
 
-        print "before createddata %r" % essfile.tell()
         globalslist.extend(parse_createddata(essfile))
-        print "after createddata %r" % essfile.tell()
 
         globalslist.extend(parse_quickkeydata(essfile))
-        print "0 %r" % essfile.tell()
 
         # reticuleData
         globalslist.extend(parse_bytelist(essfile, bytetype='s'))
-        print "1 %r" % essfile.tell()
 
         # interface stuff
         globalslist.extend(parse_bytelist(essfile))
-        print "2 %r" % essfile.tell()
 
+        # regions
         globalslist.extend(parse_regions(essfile))
-        print "3 %r" % essfile.tell()
 
         g = Globals._make(globalslist)
 
@@ -241,7 +259,6 @@ def load(options):
 
         # temporary effects
         tempEffectsSize = unpack('I', essfile.read(4))[0]
-        print tempEffectsSize
         tempEffectsData = essfile.read(tempEffectsSize)
 
         # formIds
@@ -251,22 +268,68 @@ def load(options):
         # worldSpaces
         worldSpacesNum = unpack('I', essfile.read(4))[0]
         worldSpaces = unpack('%sI' % worldSpacesNum, essfile.read(4 * worldSpacesNum))
-        print '%r %r' % (worldSpacesNum, len(worldSpaces))
 
     savegame = SaveGame._make([h, s, g, plugins, records, tempEffectsData, formIds, worldSpaces])
     return savegame
+
+def write(savegame, options):
+    with open(options.write_to, 'wb') as essfile:
+        # Fileheader
+        essfile.write(savegame.fileheader.fileId)
+        essfile.write(pack('BB', savegame.fileheader.majorVersion,
+                           savegame.fileheader.majorVersion))
+        stime = time_to_win_systemtime(savegame.fileheader.exeTime)
+        print "stime: %r" % stime
+        essfile.write(pack('8H', *stime))
+
+        # SaveGameHeader
+        essfile.write(pack('3I', savegame.gameheader.headerVersion,
+                           savegame.gameheader.saveHeaderSize,
+                           savegame.gameheader.saveNum))
+        write_b_or_bzstring(essfile, savegame.gameheader.pcName, bz=True)
+        essfile.write(pack('H', savegame.gameheader.pcLevel))
+        write_b_or_bzstring(essfile, savegame.gameheader.pcLocation, bz=True)
+        essfile.write(pack('fI', savegame.gameheader.gameDays,
+                           savegame.gameheader.gameTicks))
+
+        gtime = time_to_win_systemtime(savegame.gameheader.gameTime)
+        essfile.write(pack('8H', *gtime))
+        im = savegame.gameheader.screenshot
+        w, h = im.size
+        essfile.write(pack('3I', (w*h*3)+8, w, h))
+        essfile.write(im.tostring())
+
+        return
+        # Plugins
+        plugins = list()
+        plugincount = unpack('B', essfile.read(1))[0]
+        for index in range(plugincount):
+            plugins.append(parse_b_or_bzstring(essfile))
+
+        # Globals
+        globalslist = list()
+        globalslist.extend(list(unpack('6I', essfile.read(24))))
+        pcloc = PCLocation._make(unpack('I3f', essfile.read(16)))
+        globalslist.append(pcloc)
+        globalslist.extend(parse_globals(essfile))
+        tesClassSize = unpack('H', essfile.read(2))[0]
+        globalslist.append(tesClassSize)
+        globalslist.extend(parse_deathcounts(essfile))
+        globalslist.append(unpack('f', essfile.read(4))[0])
 
 if __name__ == '__main__':
     options = get_options()
     savegame = load(options)
 
-    print "%s plugins found" % len(savegame.plugins)
+    print savegame.gameheader
+    #print "%s plugins found" % len(savegame.plugins)
     if options.list_plugins:
         for p in sorted(savegame.plugins):
             print p
-    print "%s change records found" % len(savegame.records)
+    #print "%s change records found" % len(savegame.records)
 
-    for r in savegame.records:
-        print RecordTypeNames.get(r[1], '%r xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' % r[1])
-
-
+    #for r in savegame.records:
+    #    print RecordTypeNames.get(r[1], '%r xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' % r[1])
+    print '-----------------------------------------------'
+    if options.write_to:
+        write(savegame, options)
